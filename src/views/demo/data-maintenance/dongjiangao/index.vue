2c33564fb62181e590108f78a135243a3a856342
Eric <330886959@qq.com>
2021-01-04 20:27:45 +0800

    补充空页面

---
+++ src/views/demo/data-maintenance/dongjiangao/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="components-container">
+    董监高维护
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'DongJianGaoIndex',
+  data() {
+    return {
+    }
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
+
